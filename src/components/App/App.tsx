9706b973d22425b4310d2aaf41b39a844d1c993e
Mark <mark@remarkablemark.org>
2025-08-02 11:49:13 -0400

    feat: add icebreaker questions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,14 @@
-import { useState } from 'preact/hooks';
+import questions from './questions.json';
 
-import brands from './brands';
+const questionsLength = questions.length;
+
+const randomQuestion = (): string =>
+  questions[Math.floor(Math.random() * questionsLength)].question;
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div class="flex justify-center">
-        {brands.map(({ alt, href, src }) => (
-          <a href={href} rel="nofollow noopener" target="_blank">
-            <img src={src} class="m-4 h-24 hover:drop-shadow-2xl" alt={alt} />
-          </a>
-        ))}
-      </div>
-
-      <h1 class="my-10 text-5xl font-bold">Vite + Preact + Tailwind</h1>
-
-      <div class="p-8">
-        <button
-          class="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 text-center text-sm font-medium text-slate-800 shadow-xs transition-all hover:border-slate-800 focus:border-slate-800 focus:bg-slate-50 active:border-slate-800 active:bg-slate-50 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          onClick={() => setCount((count) => count + 1)}
-          type="button"
-        >
-          count is {count}
-        </button>
-
-        <p class="my-4 text-slate-600">
-          Edit <code class="font-[monospace]">src/components/App/App.tsx</code>{' '}
-          and save to test HMR
-        </p>
-      </div>
-
-      <p class="text-slate-400">
-        Click on the Vite, Preact, and Tailwind logos to learn more
-      </p>
-    </>
+    <h1 class="text-center font-serif text-5xl text-stone-900 sm:text-6xl lg:text-7xl dark:text-stone-100">
+      {randomQuestion()}
+    </h1>
   );
 }
